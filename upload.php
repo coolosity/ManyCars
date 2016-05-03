591741d768cd52d1a604de7c56361da2f45ab6b0
wolfram <epsiewert@comcast.net>
2016-05-03 19:47:02 -0400

    Commitments were made

--- upload.php
+++ upload.php
@@ -1,5 +1,5 @@
 <?php
 include 'connect.php';
-$sql = 'INSERT INTO Leaderboard VALUES ($_POST["score"], $_POST["id"])';
+$sql = 'INSERT INTO Leaderboard (score, id) VALUES ($_POST["score"], $_POST["id"])';
 $rs = $conn->query($sql);
 ?>
